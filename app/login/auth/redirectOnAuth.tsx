e737e8cc90bb24aaf9a33fb3c491c57b4f6226e0
stevenewald <stevenewald6@gmail.com>
2023-08-13 21:27:17 -0400

    More auth stuff

--- app/login/auth/redirectOnAuth.tsx
+++ app/login/auth/redirectOnAuth.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { useUserInfo } from "@/app/login/auth/context";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { UserInfoType, useUserInfo } from "@/app/login/auth/context";
+import { useFirebase } from "@/app/firebase/context";
+import { child, get, ref } from "firebase/database";
 
 export default function RedirectOnAuth() {
   const { user } = useUserInfo();
   const router = useRouter();
+  const { database } = useFirebase();
+  const { setUser } = useUserInfo();
   useEffect(() => {
     if (user) {
       if (user?.hasCompletedReg) {
@@ -15,5 +20,34 @@ export default function RedirectOnAuth() {
       }
     }
   }, [user]);
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, async (user) => {
+    if (!user) return;
+    const uid = user.uid;
+    const snapshot = await get(child(ref(database), `users/${uid}`));
+    if (!snapshot.exists()) {
+      //create new user
+      const newUser: UserInfoType = {
+        uid: uid,
+        displayName: user.displayName || "",
+        photoURL: user.photoURL || "",
+        email: user.email || "",
+        hasCompletedReg: false,
+      };
+      setUser(newUser);
+    } else {
+      const dbUser = snapshot.val();
+      const newUser: UserInfoType = {
+        uid: uid,
+        displayName: dbUser.displayName || "",
+        photoURL: dbUser.photoURL || "",
+        email: dbUser.email || user.email || "",
+        hasCompletedReg: dbUser.hasCompletedReg || false,
+      };
+      setUser(newUser);
+    }
+  });
+
   return <></>;
 }
---
+++ app/registration/header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="bg-gray-900 px-6 pt-24 sm:pt-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          Finish your registration
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-300">
+          Complete your registration to join the Northwestern Trading
+          Competition and showcase your skills.
+        </p>
+      </div>
+    </div>
+  );
+}
