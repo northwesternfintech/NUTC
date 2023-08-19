474477f8e9813ba61a8cf285f2cd43ad4edb06a7
Nino Maruszewski <nino.maruszewski@gmail.com>
2023-08-19 09:36:25 -0500

    test: use gtest

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Recipe(ConanFile):
         self.folders.generators = "conan"
 
     def requirements(self):
-        self.requires("fmt/9.1.0")
+        self.requires("fmt/10.1.0")
 
     def build_requirements(self):
-        self.test_requires("catch2/3.3.1")
+        self.test_requires("gtest/1.14.0")
