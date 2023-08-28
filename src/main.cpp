2e7a9e50a195bc5a9c00e84cc75a1e9fae741454
stevenewald <stevenewald6@gmail.com>
2023-08-28 15:39:38 -0400

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,14 @@
 
 #include <rabbitmq-c/amqp.h>
 
+namespace rmq = nutc::rabbitmq;
+
 int
 main()
 {
     auto const lib = library{};
 
-    nutc::rabbitmq::RabbitMQ conn;
+    rmq::RabbitMQ conn;
 
     if (!conn.initializeConnection()) {
         log_e(rabbitmq, "Failed to initialize connection");
@@ -25,12 +27,19 @@ main()
     nutc::logging::init(quill::LogLevel::TraceL3);
 
     if (num_clients == 0) {
-        log_e(client_spawning, "Spawned 0 clients");
+        log_c(client_spawning, "Spawned 0 clients");
         return 1;
     };
 
     conn.wait_for_clients(num_clients);
-    conn.consumeMessage();
+    std::variant<rmq::InitMessage, rmq::MarketOrder, rmq::RMQError> mess =
+        conn.consumeMessage();
+    if (std::holds_alternative<rmq::MarketOrder>(mess)) {
+        std::string buffer;
+        rmq::MarketOrder order = std::get<rmq::MarketOrder>(mess);
+        glz::write<glz::opts{.prettify = true}>(order, buffer);
+        log_i(main, "Received market order: {}", buffer);
+    }
     conn.closeConnection();
 
     return 0;
