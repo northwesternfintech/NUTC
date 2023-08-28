1f23c9853cc6907a173297b0dc0af2a60b086cd5
stevenewald <stevenewald6@gmail.com>
2023-08-27 21:39:55 -0400

    Added downloading algos

--- src/firebase/fetching.cpp
+++ src/firebase/fetching.cpp
@@ -4,26 +4,13 @@ namespace nutc {
 namespace client {
 
 void
-print_algo_info(const glz::json_t& algo)
+print_algo_info(const glz::json_t& algo, const std::string& algo_id)
 {
     log_i(firebase, "Running {}", algo["name"].get<std::string>());
     log_i(firebase, "Description: {}", algo["description"].get<std::string>());
     log_i(firebase, "Upload date: {}", algo["uploadDate"].get<std::string>());
     log_d(firebase, "Downloading at url {}", algo["downloadURL"].get<std::string>());
-}
-
-void
-get_most_recent_algo(const std::string& uid)
-{
-    glz::json_t user_info = get_user_info(uid);
-    // if not has "algos"
-    if (!user_info.contains("algos") || !user_info.contains("latestAlgoId")) {
-        log_e(firebase, "User has no algos");
-        exit(1);
-    }
-    std::string latestAlgoId = user_info["latestAlgoId"].get<std::string>();
-    glz::json_t algo_info = user_info["algos"][latestAlgoId];
-    print_algo_info(algo_info);
+  log_i(firebase, "Algo id: {}", algo_id);
 }
 
 glz::json_t
@@ -41,6 +28,49 @@ write_callback(void* contents, size_t size, size_t nmemb, void* userp)
 
     str->append(data, size * nmemb);
     return size * nmemb;
+}
+
+std::string
+storage_request(const std::string& firebase_url)
+{
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, firebase_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res)
+                      << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    return readBuffer;
+}
+
+void
+get_most_recent_algo(const std::string& uid)
+{
+    glz::json_t user_info = get_user_info(uid);
+    // if not has "algos"
+    if (!user_info.contains("algos") || !user_info.contains("latestAlgoId")) {
+        log_e(firebase, "User has no algos");
+        exit(1);
+    }
+    std::string latestAlgoId = user_info["latestAlgoId"].get<std::string>();
+    glz::json_t algo_info = user_info["algos"][latestAlgoId];
+    std::string downloadURL = algo_info["downloadURL"].get<std::string>();
+    print_algo_info(algo_info, latestAlgoId);
+    std::string algo_file = storage_request(downloadURL);
+  log_i(firebase, "{}", algo_file);
 }
 
 glz::json_t
--- src/firebase/fetching.hpp
+++ src/firebase/fetching.hpp
@@ -13,9 +13,12 @@
 namespace nutc {
 namespace client {
 
+//database request - change name
 glz::json_t firebase_request(
     const std::string& method, const std::string& url, const std::string& data = ""
 );
+
+std::string storage_request(const std::string& id, const std::string& url);
 
 glz::json_t get_user_info(const std::string& uid);
 
