7452bc3e8c0961f12d9aebee25c4685708fcd056
stevenewald <stevenewald6@gmail.com>
2023-09-01 22:44:30 -0400

    Change to linting colors

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -3,8 +3,8 @@ import { Fragment, useEffect, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 const statuses: any = {
   pending: "text-yellow-500 bg-yellow-100/10",
-  finished: "text-green-400 bg-green-400/10",
-  error: "text-rose-400 bg-rose-400/10",
+  success: "text-green-400 bg-green-400/10",
+  failed: "text-rose-400 bg-rose-400/10",
 };
 const environments: any = {
   Results: "text-indigo-400 bg-indigo-400/10 ring-indigo-400/20",
