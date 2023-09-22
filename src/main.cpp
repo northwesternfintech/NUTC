3ce52f0301384a4a7b905bf930eb9fff45095c6f
Nikola Maruszewski <me@nikola.cx>
2023-09-22 08:54:43 -0500

    fix: initialize logging first
    also added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -29,25 +29,31 @@ handle_sigint(int sig)
 int
 main()
 {
-    auto const lib = library{};
+    // Set up logging
+    nutc::logging::init(quill::LogLevel::TraceL3);
+
+    // Initialize signal handler
     signal(SIGINT, handle_sigint);
 
+    // Connect to RabbitMQ
     if (!conn.initializeConnection()) {
         log_e(rabbitmq, "Failed to initialize connection");
         return 1;
     }
 
+    // Get users from firebase
     glz::json_t::object_t firebase_users = nutc::client::get_all_users();
     users.initialize_from_firebase(firebase_users);
-    int num_clients = nutc::client::spawn_all_clients(users);
 
-    nutc::logging::init(quill::LogLevel::TraceL3);
+    // Spawn clients
+    int num_clients = nutc::client::spawn_all_clients(users);
 
     if (num_clients == 0) {
         log_c(client_spawning, "Spawned 0 clients");
         return 1;
     };
 
+    // Run exchange
     conn.waitForClients(num_clients);
     conn.handleIncomingMessages();
     conn.closeConnection();
