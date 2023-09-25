f32e069bb945d1f30713fd2fdb765ee7eef3edc6
stevenewald <stevenewald6@gmail.com>
2023-09-25 17:15:42 +0100

    More invalid funds tests

--- test/src/invalid_orders.cpp
+++ test/src/invalid_orders.cpp
@@ -33,7 +33,7 @@ TEST_F(InvalidOrders, SimpleInvalidFunds)
     EXPECT_EQ(err.value(), nutc::messages::BUY);
 }
 
-TEST_F(InvalidOrders, MatchingInvalidFunds)
+TEST_F(InvalidOrders, RemoveThenAddFunds)
 {
     manager.modifyCapital("ABC", -1000);
 
@@ -61,4 +61,22 @@ TEST_F(InvalidOrders, MatchingInvalidFunds)
     auto [matches4, ob_updates4] = engine.match_order(order1, manager);
     EXPECT_EQ(matches4.size(), 1);
     EXPECT_EQ(ob_updates4.size(), 1);
+}
+
+TEST_F(InvalidOrders, MatchingInvalidFunds)
+{
+    manager.modifyCapital("ABC", -1000);
+
+    MarketOrder order1{"ABC", nutc::messages::BUY, "MARKET", "ETHUSD", 1, 1};
+    MarketOrder order2{"DEF", nutc::messages::SELL, "MARKET", "ETHUSD", 1, 1};
+
+    // Thrown out
+    auto [matches, ob_updates] = engine.match_order(order1, manager);
+    EXPECT_EQ(matches.size(), 0);
+    EXPECT_EQ(ob_updates.size(), 0);
+
+    // Kept, but not matched
+    auto [matches2, ob_updates2] = engine.match_order(order2, manager);
+    EXPECT_EQ(matches2.size(), 0);
+    EXPECT_EQ(ob_updates2.size(), 1);
 }
