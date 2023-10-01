be31a1c63479da7be59401abf2f065d4cd458871
stevenewald <stevenewald6@gmail.com>
2023-10-01 17:18:12 +0100

    Removed type from market order

--- src/mock_api/mock_api.cpp
+++ src/mock_api/mock_api.cpp
@@ -3,21 +3,19 @@
 namespace nutc {
 namespace mock_api {
 std::function<
-    bool(const std::string&, const std::string&, const std::string&, float, float)>
+    bool(const std::string&, const std::string&, float, float)>
 
 getMarketFunc()
 {
     return [](const std::string& side,
-              const std::string& type,
               const std::string& ticker,
               float quantity,
               float price) {
         log_i(
             mock_api,
-            "Mock API: Placing order side {} type {} ticker {} quantity {} price "
+            "Mock API: Placing order side {} ticker {} quantity {} price "
             "{}",
             side,
-            type,
             ticker,
             quantity,
             price
--- src/mock_api/mock_api.hpp
+++ src/mock_api/mock_api.hpp
@@ -9,7 +9,7 @@ namespace nutc {
 namespace mock_api {
 
 std::function<
-    bool(const std::string&, const std::string&, const std::string&, float, float)>
+    bool(const std::string&, const std::string&, float, float)>
 getMarketFunc();
 }
 } // namespace nutc
