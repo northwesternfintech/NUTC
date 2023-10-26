5ca66334ba654b3ed7846505bb281400c81ce20b
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-10-26 14:08:09 -0500

    quic fix

--- web/app/registration/page.tsx
+++ web/app/registration/page.tsx
@@ -276,7 +276,7 @@ export default function Registration() {
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
