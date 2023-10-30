caccce1639d15a3f553f2de7936361d05f33778b
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-10-29 23:07:05 -0500

    remove tsq

--- linter/src/main.cpp
+++ linter/src/main.cpp
@@ -1,6 +1,5 @@
 #define CROW_MAIN
 #include "common.hpp"
-#include "thread_safe_queue/tsq.hpp"
 
 #include <argparse/argparse.hpp>
 #include <crow/app.h>
@@ -86,9 +85,7 @@ main(int argc, const char** argv)
     nutc::logging::init(verbosity);
     log_i(main, "Starting NUTC Linter");
 
-    nutc::tsq::ThreadSafeQueue<std::pair<std::string, std::string>> queue;
-
-    std::thread server_thread([&queue]() {
+    std::thread server_thread([]() {
         crow::SimpleApp app;
         CROW_ROUTE(app, "/")
         ([&](const crow::request& req) {
@@ -104,25 +101,14 @@ main(int argc, const char** argv)
             }
             std::string uid = req.url_params.get("uid");
             std::string algo_id = req.url_params.get("algo_id");
-            queue.push(std::make_pair(uid, algo_id));
+            
+            spawn_client(uid, algo_id);
+            
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             return crow::response();
         });
         app.port(8080).run();
     });
-
-    while (true) {
-        std::optional<std::pair<std::string, std::string>> pair = queue.pop();
-        if (!pair) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            continue;
-        }
-
-        std::string uid = pair.value().first;
-        std::string algo_id = pair.value().second;
-
-        spawn_client(uid, algo_id);
-    }
 
     server_thread.join();
 
