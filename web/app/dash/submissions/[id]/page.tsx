d0968f982b50cf87cc8c670e3bdec00c6ca50cce
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-11-13 13:48:59 -0600

    change rendering of failure/success message on frontend

--- web/app/dash/submissions/[id]/page.tsx
+++ web/app/dash/submissions/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { useUserInfo } from "@/app/login/auth/context";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import React from "react";
 export default function Page({ params }: { params: { id: string } }) {
   const userInfo = useUserInfo();
   const router = useRouter();
@@ -16,10 +17,27 @@ export default function Page({ params }: { params: { id: string } }) {
       router.push("/dash");
     }
   });
-  return (
-    <h1>
-      {userInfo?.user?.algos?.get(params.id)?.lintFailureMessage ||
-        userInfo?.user?.algos?.get(params.id)?.lintSuccessMessage || ""}
-    </h1>
-  );
+
+  const formatNewLines = (str: string) => {
+    const LINES = str.split("\n");
+    return LINES.map((line: string, index: number) => (
+      <React.Fragment key={`line_${index}`}>
+        <h1>{line}</h1>
+        {index < LINES.length - 1 && <br />}
+      </React.Fragment>
+    ));
+  }
+  
+
+  const stringToRender = userInfo?.user?.algos?.get(params.id)?.lintFailureMessage ||
+  userInfo?.user?.algos?.get(params.id)?.lintSuccessMessage || "";
+  if(stringToRender === "") {
+    return <></>
+  } else {
+    return (
+      <>
+        {formatNewLines(stringToRender)}
+      </>
+    );
+  }
 }
