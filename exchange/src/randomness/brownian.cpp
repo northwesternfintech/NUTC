c1bf1e5f1ab3b3566dbad620842fe8dc2eb097fc
Andrew Li <54134293+andrlime@users.noreply.github.com>
2024-01-12 10:48:56 -0600

    create boilerplate code for brownian motion

---
+++ exchange/src/randomness/brownian.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file brownian.cpp
+ * @author Andrew Li (andrewli@u.northwestern.edu)
+ * @brief Brownian motion randomness to simulate market chaos
+ * @version 0.1
+ * @date 2024-01-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "brownian.hpp";
+
+namespace nutc {
+namespace brownian {
+
+float
+BrownianMotion::generate_next_price()
+{
+
+}
+
+}
+}
