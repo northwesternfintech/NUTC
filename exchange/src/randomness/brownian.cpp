582ab76f2f0827014970f2b20a513bf06fd38e2e
Andrew Li <54134293+andrlime@users.noreply.github.com>
2024-01-12 21:09:14 -0600

    fix formatting

--- exchange/src/randomness/brownian.cpp
+++ exchange/src/randomness/brownian.cpp
@@ -4,9 +4,9 @@
  * @brief Brownian motion randomness to simulate market chaos
  * @version 0.1
  * @date 2024-01-12
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include "brownian.hpp"
@@ -18,8 +18,10 @@ double
 BrownianMotion::generate_next_price()
 {
     double current_price = this->cur_value;
-    std::normal_distribution<double> distribution(-this->cur_value/1000, BROWNIAN_MOTION_DEVIATION);
-    
+    std::normal_distribution<double> distribution(
+        -this->cur_value / 1000, BROWNIAN_MOTION_DEVIATION
+    );
+
     double delta_current_price = distribution(this->random_number_generator);
     double new_price = current_price + delta_current_price;
 
@@ -27,5 +29,5 @@ BrownianMotion::generate_next_price()
     return new_price;
 }
 
-}
-}
+} // namespace brownian
+} // namespace nutc
