58d6f67cf52ca0ac7071dddc523e9ab99877be3f
Andrew Li <54134293+andrlime@users.noreply.github.com>
2024-01-12 20:57:35 -0600

    fix compile time errors

--- exchange/src/randomness/brownian.hpp
+++ exchange/src/randomness/brownian.hpp
@@ -21,28 +21,27 @@ namespace brownian {
 const double BROWNIAN_MOTION_DEVIATION = 0.4;
 
 class BrownianMotion {
-    int seed;
+    unsigned int seed;
     double cur_value;
-    double deviation;
 
     std::mt19937 random_number_generator;
 
 public:
     // dConstructor for BrownianMotion, takes nothing
-    BrownianMotion() : deviation(BROWNIAN_MOTION_DEVIATION) {
+    BrownianMotion() {
         std::random_device rd;
         random_number_generator = std::mt19937(rd());
-    };
+    }
 
     // Constructor for BrownianMotion, takes a seed
-    BrownianMotion(const int seed) : seed(seed), deviation(BROWNIAN_MOTION_DEVIATION) {
+    BrownianMotion(const unsigned int seed) : seed(seed) {
         random_number_generator = std::mt19937(seed);
-    };
+    }
 
     // Constructor for BrownianMotion, takes a seed and initial value
-    BrownianMotion(const int seed, const double initial_value) : seed(seed), cur_value(initial_value), deviation(BROWNIAN_MOTION_DEVIATION) {
+    BrownianMotion(const unsigned int seed, const double initial_value) : seed(seed), cur_value(initial_value) {
         random_number_generator = std::mt19937(seed);
-    };
+    }
 
     // Generates and returns the next price based on previous prices
     double
@@ -60,7 +59,7 @@ public:
     // Force set the seed to something else
     void
     force_set_seed(
-        int new_seed
+        unsigned int new_seed
     )
     {
         seed = new_seed;
