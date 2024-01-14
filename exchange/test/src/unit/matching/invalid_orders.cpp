8e145894db88364aedda361cdf4e4cf7182da3ea
stevenewald <stevenewald6@gmail.com>
2024-01-14 10:44:41 -0800

    Integration tests automatically disable wrapper start delay

--- exchange/test/src/unit/matching/invalid_orders.cpp
+++ exchange/test/src/unit/matching/invalid_orders.cpp
@@ -15,8 +15,10 @@ protected:
     void
     SetUp() override
     {
-        manager_.add_client("ABC", "ABC");
-        manager_.add_client("DEF", "DEF");
+        using nutc::testing_utils::add_client_simple;
+
+        add_client_simple(manager_, "ABC");
+        add_client_simple(manager_, "DEF");
         manager_.modify_holdings("ABC", "ETHUSD", DEFAULT_QUANTITY);
         manager_.modify_holdings("DEF", "ETHUSD", DEFAULT_QUANTITY);
     }
@@ -92,10 +94,13 @@ TEST_F(UnitInvalidOrders, MatchingInvalidFunds)
 
 TEST_F(UnitInvalidOrders, SimpleManyInvalidOrder)
 {
-    manager_.add_client("A", "A");
-    manager_.add_client("B", "B");
-    manager_.add_client("C", "C");
-    manager_.add_client("D", "D");
+    using nutc::testing_utils::add_client_simple;
+
+    add_client_simple(manager_, "A");
+    add_client_simple(manager_, "B");
+    add_client_simple(manager_, "C");
+    add_client_simple(manager_, "D");
+
     manager_.modify_capital("B", -STARTING_CAPITAL);
     manager_.modify_holdings("A", "ETHUSD", DEFAULT_QUANTITY);
     manager_.modify_holdings("B", "ETHUSD", DEFAULT_QUANTITY);
