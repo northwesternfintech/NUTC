898ec03dcbf38b829e5d3591df8ce0ecf5176e41
Steven Ewald <stevenewald6@gmail.com>
2024-02-01 21:26:51 -0800

    Fixing test paths

--- exchange/test/src/test_utils/process.hpp
+++ exchange/test/src/test_utils/process.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "exchange/client_manager/client_manager.hpp"
+#include "exchange/process_spawning/spawning.hpp"
 
 namespace nutc {
 namespace testing_utils {
@@ -8,7 +9,8 @@ namespace testing_utils {
 void kill_all_processes(const manager::ClientManager& users);
 
 void initialize_testing_clients(
-    nutc::manager::ClientManager& users, const std::vector<std::string>& algo_filenames
+    nutc::manager::ClientManager& users, const std::vector<std::string>& algo_filenames,
+    client::SpawnMode mode = client::SpawnMode::TESTING
 );
 
 } // namespace testing_utils
