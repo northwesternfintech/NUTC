31104fd3f86fc47471dbe84feb0a5aa81e341911
Steven Ewald <stevenewald6@gmail.com>
2024-02-04 12:58:24 -0800

    Moved theo generation to bot container

--- exchange/src/exchange/bot_framework/bot_container.hpp
+++ exchange/src/exchange/bot_framework/bot_container.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include "exchange/bot_framework/bots/mm.hpp"
+#include "exchange/randomness/brownian.hpp"
+#include "exchange/tick_manager/tick_observer.hpp"
 #include "shared/messages_exchange_to_wrapper.hpp"
+
+#include <iostream>
 
 namespace nutc {
 
@@ -8,8 +12,15 @@ namespace bots {
 using MarketOrder = messages::MarketOrder;
 using Match = messages::Match;
 
-class BotContainer {
+class BotContainer : public ticks::TickObserver {
 public:
+    void on_tick() override
+    {
+        auto theo = theo_generator_.generate_next_price();
+        // std::cout << "Theo: " << theo << "\n";
+        auto orders = BotContainer::on_new_theo(static_cast<float>(theo));
+    }
+
     void
     add_mm_bot(std::string& bot_id, MarketMakerBot&& bot)
     {
@@ -21,7 +32,7 @@ public:
     {
         std::vector<MarketOrder> orders;
         for (auto& [id, bot] : market_makers_) {
-            auto bot_orders = bot.take_action(new_theo);
+      std::vector<messages::MarketOrder> bot_orders = bot.take_action(new_theo);
             orders.insert(orders.end(), bot_orders.begin(), bot_orders.end());
         }
         return orders;
@@ -47,8 +58,10 @@ private:
     // TODO(stevenewald): make more elegant than string UUID
     std::unordered_map<std::string, MarketMakerBot> market_makers_;
 
+    stochastic::BrownianMotion theo_generator_{};
+
     BotContainer() = default;
-    ~BotContainer() = default;
+    ~BotContainer() override = default;
 
 public:
     // Singleton functionality
