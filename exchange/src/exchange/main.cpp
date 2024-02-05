204d00419552ea9be6746477e20312108ef2dc56
Steven Ewald <stevenewald6@gmail.com>
2024-02-04 16:03:11 -0800

    Fixed order expiration and other changes

--- exchange/src/exchange/main.cpp
+++ exchange/src/exchange/main.cpp
@@ -115,14 +115,17 @@ initialize_ticker(const std::string& ticker, float starting_price)
 {
     using nutc::bots::BotContainerMapper;
     using nutc::engine_manager::EngineManager;
+    using nutc::ticks::PRIORITY;
     using nutc::ticks::TickManager;
 
     auto& tick_manager = TickManager::get_instance();
     EngineManager::get_instance().add_engine(ticker);
     EngineManager::get_instance().set_initial_price(ticker, starting_price);
 
-    auto& tick_listener = BotContainerMapper::get_instance(ticker, starting_price);
-    tick_manager.attach(&tick_listener);
+    auto& bot_container = BotContainerMapper::get_instance(ticker, starting_price);
+
+    // Should run after stale order removal, so they can react to removed orders
+    tick_manager.attach(&bot_container, PRIORITY::second);
 }
 
 int
@@ -145,6 +148,8 @@ main(int argc, const char** argv)
     bots::MarketMakerBot bot1{5000};
     bots::BotContainerMapper::get_instance("A").add_mm_bot("MM_BOT_1", bot1);
 
+    auto& engine_manager = engine_manager::EngineManager::get_instance();
+    ticks::TickManager::get_instance().attach(&engine_manager, ticks::PRIORITY::first);
     ticks::TickManager::get_instance().start();
 
     manager::ClientManager users;
@@ -203,9 +208,6 @@ main(int argc, const char** argv)
     }
 
     client::spawn_all_clients(users);
-
-    engine_manager::EngineManager& engine_manager =
-        nutc::engine_manager::EngineManager::get_instance();
 
     // Run exchange
     rabbitmq::RabbitMQClientManager::wait_for_clients(users, num_clients);
--- exchange/test/src/unit/misc/tick_manager.cpp
+++ exchange/test/src/unit/misc/tick_manager.cpp
@@ -10,6 +10,7 @@ class UnitTickManagerTest : public ::testing::Test {
     using TickManager = nutc::ticks::TickManager;
 
 protected:
+    using PRIORITY = nutc::ticks::PRIORITY;
     static constexpr uint16_t START_TICK_RATE = 100;
 
     void
@@ -31,31 +32,41 @@ TEST_F(UnitTickManagerTest, SingletonInstance)
 class TestObserver : public nutc::ticks::TickObserver {
 public:
     void
-    on_tick() override
+    on_tick(uint64_t tick) override
     {
         tick_count_++;
+        current_tick_ = tick;
     }
 
     uint32_t tick_count_ = 0;
+    uint64_t current_tick_ = 0;
 
     uint32_t
     get_tick_count()
     {
         return tick_count_;
     }
+
+    uint64_t
+    get_current_tick()
+    {
+        return current_tick_;
+    }
 };
 
 TEST_F(UnitTickManagerTest, AttachDetachObserver)
 {
     TestObserver observer;
-    manager_.attach(&observer);
+    manager_.attach(&observer, PRIORITY::first);
     manager_.start();
     // wait for 100 ms, should be around 10 ticks
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     manager_.stop();
-    manager_.detach(&observer);
+    manager_.detach(&observer, PRIORITY::first);
     ASSERT_GE(observer.get_tick_count(), 8);
     ASSERT_LE(observer.get_tick_count(), 12);
+    ASSERT_GE(observer.get_current_tick(), 8);
+    ASSERT_LE(observer.get_current_tick(), 12);
 }
 
 // NOLINTEND
