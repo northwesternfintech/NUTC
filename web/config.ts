81436aa6312e444c0bdecad788c97fb68d58c24c
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-02-29 12:53:04 -0600

    Created docker for sandbox and created sandbox endpoint in FE

--- web/config.ts
+++ web/config.ts
@@ -2,6 +2,7 @@ import { isLocalhost } from "@/app/firebase/config";
 
 export const config: any = {
   linterBaseUrl: "https://nutc-linter-4oeeau4rxa-uc.a.run.app",
+  sandboxBaseUrl: "",
 };
 
 export function linterEndpoint(uid: string, algo_id: string): string {
@@ -9,5 +10,13 @@ export function linterEndpoint(uid: string, algo_id: string): string {
     return `http://127.0.0.1:8080/?uid=${uid}&algo_id=${algo_id}`;
   } else {
     return `${config.linterBaseUrl}/?uid=${uid}&algo_id=${algo_id}`;
+  }
+}
+
+export function sandboxEndpoint(uid: string, algo_id: string): string {
+  if (isLocalhost()) {
+    return `http://127.0.0.1:8081/?user_id=${uid}&algo_id=${algo_id}`;
+  } else {
+    return `${config.sandboxBaseUrl}/?user_id=${uid}&algo_id=${algo_id}`;
   }
 }
