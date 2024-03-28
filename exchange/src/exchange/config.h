e87c9c6d7ce7a36504a38223896e1c72540fd67e
Andrew Li <andrewli@u.northwestern.edu>
2024-03-27 23:31:43 -0500

    move brownian constants to a sub-namespace with comments

--- exchange/src/exchange/config.h
+++ exchange/src/exchange/config.h
@@ -29,12 +29,52 @@
 
 enum class Mode { SANDBOX, DEV, NORMAL, BOTS_ONLY };
 
-// Brownian motion stuff
- #define BROWNIAN_MOTION_MEAN_SIZE_EVENT  15
- #define BROWNIAN_MOTION_STDEV_EVENT_SIZE 5
- #define BROWNIAN_MOTION_DEVIATION        0.1
- #define SKEW_SCALE                       20000 // Factor by which the market wants to return to baseline (larger = less)
- #define SKEW_FACTOR                      4 // How much more market events skew than normal
+/** Brownian motion parameters **/
+#ifndef uint
+    typedef unsigned int uint; //  stupid hack
+#endif
+namespace BrownianMotionParameters {
+    /**
+     * `DEVIATION`
+     * Under the brownian motion model, d(price)/d(tick) is a normal distribution
+     * around some mean. That mean is determined mathematically: either zero, or
+     * skewed towards zero using `TICK_SKEW_SCALE`. This variable is the stdev.
+     * 
+     * Higher means more change per tick for all ticks; lower means less. This affects
+     * market events too, which have a stdev = `DEVIATION` * `EVENT_DEVIATION_FACTOR`
+     */
+    constexpr double DEVIATION = 0.1;
+    
+    /**
+     * `EVENT_SIZE_MEAN`: Average length, in ticks, of a market event
+     * `EVENT_SIZE_STDEV`: Stdev length of a market event
+     * `EVENT_DEVIATION_FACTOR`: Market events are themselves long chains of normal distributions
+     *                           that are unsigned and multiplied by either + or -. The stdev
+     *                           equals `DEVIATION` * `EVENT_DEVIATION_FACTOR`
+     * 
+     * It should be quite trivial how to manipulate the first two variables. The third requires more experimentation.
+     * A higher deviation factor makes market events inherently more drastic than non market events. A smaller factor
+     * means they are closer to "the status quo" and don't really do anything different.
+     */
+    constexpr uint EVENT_SIZE_MEAN = 15;
+    constexpr uint EVENT_SIZE_STDEV = 5;
+    constexpr uint EVENT_DEVIATION_FACTOR = 4;
+    
+    /**
+     * `TICK_SKEW_SCALE`
+     * Skew scale is a hacky solution to avoid long term divergence, as if we treat brownian motion
+     * as a series of normal distributions, as we approach infinitely many terms, we just get more and
+     * more variability. If we want to force the brownian motion to stay somewhat close to baseline, we
+     * skew it.
+     * 
+     * The way this works is by directly affecting the mean of d(price)/d(tick). Mean is no longer zero,
+     * but rather -(current value)/(tick skew scale) to drag it closer to baseline by a little bit.
+     * 
+     * Because this is inverse, higher means more able to diverge over time;
+     * smaller means more constrained to be close to baseline.
+     */
+    constexpr uint TICK_SKEW_SCALE = 20000;
+}
 
 
 #define RETAIL_ORDER_OFFSET .02 // How much retail orders are offset from theo price. Ex. .02 means buy order is theo+.02 to ensure it gets filled
