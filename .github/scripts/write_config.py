9dcb9e6dacdfbd601b261275250274a7ba69b842
Andrew Li <andrewli@u.northwestern.edu>
2024-04-06 11:30:58 -0500

    implement darwin CI/CD for linter and exchange

---
+++ .github/scripts/write_config.py
@@ -0,0 +1,18 @@
+import os
+
+content = """
+[settings]
+arch=armv8
+build_type=Release
+compiler=clang
+compiler.cppstd=gnu20
+compiler.libcxx=libc++
+compiler.version=17
+os=Macos
+"""
+
+filepath = os.path.expanduser("~/.conan2/profiles/default")
+
+f = open(filepath, "w")
+f.write(content)
+f.close()
