3160e46a1abeefba17c63453f020a7c322b777c8
stevenewald <stevenewald6@gmail.com>
2024-05-03 01:08:32 -0500

    tmp

--- exchange/src/exchange/bots/bot_types/bollinger.hpp
+++ exchange/src/exchange/bots/bot_types/bollinger.hpp
@@ -15,17 +15,12 @@ namespace nutc {
 namespace bots {
 
 class BollingerBot : public traders::BotTrader {
-    std::random_device rd{};
-    std::mt19937 gen{rd()}; // not really sure what this does/if it's necessary
-                            // auto generate aggresiveness?
-
     std::deque<double> price_history{};
     const std::size_t sma_period;
     const double multiplier;
 
     double current_sma = 0.0;
     double current_variance = 0.0;
-    std::size_t num_data_points = 0;
 
 public:
     BollingerBot(
@@ -37,8 +32,6 @@ public:
     {}
 
     bool constexpr can_leverage() const override { return true; }
-
-    [[nodiscard]] bool is_active() const override;
 
     std::optional<messages::market_order> take_action(double current, double theo);
 
