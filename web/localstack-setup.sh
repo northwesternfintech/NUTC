4c28cbf65d17c6cc1d64a8b3864717b68fdc74b4
Steven Ewald <stevenewald6@gmail.com>
2024-09-08 15:25:15 -0400

    Checkpoint

--- web/localstack-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws --endpoint-url=http://localhost:4566 s3 mb s3://nutc
-aws --endpoint-url=http://localhost:4566 s3api put-bucket-cors --bucket nutc --cors-configuration file://cors.json
-
-#aws --endpoiunt-url=http://localhost:4566 s3 ls s3://nutc --recursive
