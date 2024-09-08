d0eb3acdf80154462642b87b435500b1f5048313
Max Glass <glassmax13@gmail.com>
2024-09-08 18:34:00 -0400

    add state to user

--- web/app/dash/layout.tsx
+++ web/app/dash/layout.tsx
@@ -1,17 +1,18 @@
-import { getAlgos } from "@/api";
+import { getAlgos, redirectIfStateNot } from "@/api";
 import { DashNav } from "./dash-nav";
 import { redirect } from "next/navigation";
+import { ParticipantState } from "@prisma/client";
 
 export default async function DashLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const algos = await getAlgos();
+  await redirectIfStateNot(ParticipantState.ACCEPTED);
 
+  const algos = await getAlgos();
   if (!algos) {
     redirect("/api/auth/login");
   }
-
   return <DashNav algos={algos}>{children}</DashNav>;
 }
