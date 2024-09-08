7bb114cf555a853556dabeeb6d30d55512de294c
Max Glass <glassmax13@gmail.com>
2024-09-08 18:34:00 -0400

    add state to user

--- web/api.tsx
+++ web/api.tsx
@@ -2,6 +2,49 @@
 
 import { getSession } from "@auth0/nextjs-auth0";
 import prisma from "@/prisma/prismaClient";
+import { redirect } from "next/navigation";
+import { ParticipantState } from "@prisma/client";
+
+export async function getUserRegistrationState() {
+  const session = await getSession();
+  if (!session?.user.sub) {
+    return null;
+  }
+
+  const uid: string = session.user.sub;
+  if (!uid) {
+    return null;
+  }
+
+  const res = await prisma.user.findUnique({
+    where: {
+      uid: uid,
+    },
+    select: {
+      participantState: true,
+    },
+  });
+
+  return res?.participantState;
+}
+
+export async function redirectIfStateNot(state: ParticipantState) {
+  const userRegistrationState = await getUserRegistrationState();
+  if (!userRegistrationState) {
+    redirect("/api/auth/login");
+  }
+  if (userRegistrationState === state) return;
+
+  if (userRegistrationState === ParticipantState.WAITING) {
+    redirect("/waiting");
+  } else if (userRegistrationState === ParticipantState.PRE_REGISTRATION) {
+    redirect("/register");
+  } else if (userRegistrationState === ParticipantState.REJECTED) {
+    redirect("/rejected");
+  } else {
+    redirect("/dash");
+  }
+}
 
 export async function getAlgos() {
   const session = await getSession();
--- web/app/api/protected/db/user/createUser/route.ts
+++ web/app/api/protected/db/user/createUser/route.ts
@@ -1,40 +1,45 @@
 import prisma from "@/prisma/prismaClient";
 import { getSession } from "@auth0/nextjs-auth0";
-import { Profile } from "@prisma/client";
+import { ParticipantState, Profile } from "@prisma/client";
 import { NextResponse } from "next/server";
 import createDefaultUser from "../defaultUser";
 import { SendEmail } from "@/app/api/sendEmail";
 import { GenerateApplicationEmail } from "./generateApplicationEmail";
 
 export async function POST(req: Request) {
-	try {
-		const profile: Profile = await req.json();
-		console.log(JSON.stringify(profile));
-		for (const [_, value] of Object.entries(profile)) {
-			if (!value) {
-				return NextResponse.json(
-					{ message: "Not all fields in profile added" },
-					{ status: 402 },
-				);
-			}
-		}
+  try {
+    const profile: Profile = await req.json();
+    console.log(JSON.stringify(profile));
+    for (const [_, value] of Object.entries(profile)) {
+      if (!value) {
+        return NextResponse.json(
+          { message: "Not all fields in profile added" },
+          { status: 402 },
+        );
+      }
+    }
 
-		const session = await getSession();
-		if (!session?.user.sub || session.user.sub != profile.uid) {
-			return NextResponse.json({ message: "Unauthorized" }, { status: 402 });
-		}
-		const user = await createDefaultUser(session);
+    const session = await getSession();
+    if (!session?.user.sub || session.user.sub != profile.uid) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 402 });
+    }
+    const user = await createDefaultUser(session);
 
-		await prisma.profile.create({
-			data: profile,
-		});
+    await prisma.profile.create({
+      data: profile,
+    });
 
-		SendEmail("info@nutc.io", session?.user.email, "Application Submitted", "Your NUTC application was submitted. You will hear back in the next 72 hours.");
-		GenerateApplicationEmail(user, profile);
+    SendEmail(
+      "info@nutc.io",
+      session?.user.email,
+      "Application Submitted",
+      "Your NUTC application was submitted. You will hear back in the next 72 hours.",
+    );
+    GenerateApplicationEmail(user, profile);
 
-		return NextResponse.json({ status: 200 });
-	} catch (error) {
-		console.log(error);
-		return NextResponse.json({ message: error }, { status: 500 });
-	}
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
