15d46d0dc388667720e8958ec5330525bc1e53e3
Steven Ewald <stevenewald6@gmail.com>
2024-09-09 16:07:30 -0400

    Add node-mailer functionality

--- web/app/api/protected/db/user/createUser/generateApplicationEmail.ts
+++ web/app/api/protected/db/user/createUser/generateApplicationEmail.ts
@@ -6,12 +6,14 @@ export async function GenerateApplicationEmail(user: User, profile: Profile) {
 	const token = (await prisma.emailTokens.create({ data: { uid: user.uid } })).token;
 
 	const link = process.env.AUTH0_BASE_URL + `/handleReview?token=${token}`;
+	const accept_link = link + "&accept=true";
+	const deny_link = link + "&accept=false";
 
 	const mailOptions = {
 		from: "contact@nutc.io",
 		to: 'steveewald2025@u.northwestern.edu',
 		subject: `[ACTION REQUIRED] NUTC Application Submitted`,
-		text: `${profile.firstName} ${profile.lastName} submitted an application. Click <a href="${link}">Here</a> to accept.`
+		text: `${profile.firstName} ${profile.lastName} submitted an application. Click <a href="${accept_link}">Here</a> to accept. and <a href="${deny_link}"> here </a> to decline.`
 	};
 
 	return await SendEmail(mailOptions.from, mailOptions.to, mailOptions.subject, mailOptions.text)
