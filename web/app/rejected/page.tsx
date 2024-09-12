3ac4cff3b80601ba58d574a5a7f730c102ca972f
Steven Ewald <stevenewald6@gmail.com>
2024-09-12 13:50:51 -0400

    Add rejected and waiting pages

---
+++ web/app/rejected/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+export default function Rejected() {
+  return (
+    <>
+      <main className="relative isolate min-h-screen">
+        <Image
+          src="/images/background.jpeg"
+          alt="Background"
+          width={1500}
+          height={1125}
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Application Update</h1>
+          <p className="mt-4 text-base text-white/70 sm:mt-6">Unfortunately, you were not selected to participate in NUTC 2024. We encourage you to reapply next year.</p>
+          <div className="mt-10 flex justify-center">
+            <a href="/" className="text-sm font-semibold leading-7 text-white">
+              <span aria-hidden="true">&larr;</span> Back to home
+            </a>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
