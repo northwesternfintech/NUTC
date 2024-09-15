829305a8497fa7458d3413fc07347e8e176ee128
Steven Ewald <stevenewald6@gmail.com>
2024-09-15 18:55:07 -0400

    Redirect about to nuft site

--- web/app/landing/navigation.tsx
+++ web/app/landing/navigation.tsx
@@ -1,5 +1,5 @@
 export const navigation = [
-  { name: "About", href: "/about" },
+  { name: "About", href: "https://www.nuft.dev" },
   { name: "FAQ", href: "#faq" },
   { name: "Contact", href: "/contact" },
   { name: "Apply", href: "/register" },
