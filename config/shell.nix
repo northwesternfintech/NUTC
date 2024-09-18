9777d9c8292a839a7c17164b4a3d2f582d9f30c9
jiayi-jenny-zhou <zhou_jiayi05@outlook.com>
2024-09-18 19:01:39 -0400

    delete all python packages from shell.nix besides the basic py package

--- config/shell.nix
+++ config/shell.nix
@@ -5,12 +5,6 @@ pkgs.mkShell {
   pkgs.conan
   pkgs.ncurses
   pkgs.python312Full
-  pkgs.python312Packages.cppy
-  pkgs.python312Packages.matplotlib
-  pkgs.python312Packages.numpy
-  pkgs.python312Packages.yfinance
-  pkgs.python312Packages.pandas
-  pkgs.python312Packages.scikit-learn
   pkgs.clang-tools
   pkgs.codespell
   ];
