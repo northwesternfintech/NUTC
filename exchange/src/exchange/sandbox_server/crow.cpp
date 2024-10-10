3a5506e92e7d77feef019d96b3b9faa5e558ad41
Max Glass <glassmax13@gmail.com>
2024-10-10 03:19:30 -0500

    add logging to sandbox

--- exchange/src/exchange/sandbox_server/crow.cpp
+++ exchange/src/exchange/sandbox_server/crow.cpp
@@ -1,12 +1,16 @@
 #include "crow.hpp"
 
+#include "common/fetching/fetching.hpp"
 #include "common/logging/logging.hpp"
 #include "common/messages_exchange_to_wrapper.hpp"
 #include "common/types/algorithm/base_algorithm.hpp"
 #include "exchange/config/dynamic/config.hpp"
+#include "exchange/config/static/config.hpp"
 #include "exchange/traders/trader_types/algo_trader.hpp"
 
+#include <boost/filesystem/operations.hpp>
 #include <crow/common.h>
+#include <unistd.h>
 
 namespace nutc::exchange {
 
@@ -34,17 +38,27 @@ CrowServer::CrowServer() :
                     return res;
                 }
 
+                if (!req.url_params.get("logfile_url")) {
+                    log_e(main, "No logfile_url provided");
+                    return crow::response(400);
+                }
+
+                std::string logfile_url = req.url_params.get("logfile_url");
+
                 try {
                     log_i(
                         sandbox_server,
-                        "Received sandbox request with algo_id {} and language {}",
-                        algo_id, language
+                        "Received sandbox request with algo_id {} and language {}. "
+                        "Pre-signed url: {}",
+                        algo_id, language, logfile_url
                     );
                     common::AlgoLanguage language_enum =
                         language == "Python" ? common::AlgoLanguage::python
                                              : common::AlgoLanguage::cpp;
                     std::string algorithm_data = req.body;
-                    add_pending_trader_(algo_id, language_enum, algorithm_data);
+                    add_pending_trader_(
+                        algo_id, language_enum, algorithm_data, logfile_url
+                    );
                     crow::json::wvalue response_json({
                         {"success", true                              },
                         {"message", "Algorithm Successfully Submitted"}
@@ -68,7 +82,7 @@ CrowServer::CrowServer() :
 void
 CrowServer::add_pending_trader_(
     const std::string& algo_id, common::AlgoLanguage language,
-    const std::string& algorithm_data
+    const std::string& algorithm_data, const std::string& logfile_url
 )
 {
     static const auto STARTING_CAPITAL = Config::get().constants().STARTING_CAPITAL;
@@ -82,7 +96,7 @@ CrowServer::add_pending_trader_(
     trader_lock.unlock();
 
     static auto trial_secs = Config::get().constants().SANDBOX_TRIAL_SECS;
-    start_remove_timer_(trial_secs, trader);
+    start_remove_timer_(trial_secs, trader, algo_id, logfile_url);
 
     auto get_start_message = []() {
         static auto start_message = glz::write_json(common::start_time{0});
@@ -112,17 +126,17 @@ CrowServer::~CrowServer()
 
 void
 CrowServer::start_remove_timer_(
-    unsigned int time_s, std::weak_ptr<GenericTrader> trader_ptr
+    unsigned int time_ms, std::weak_ptr<GenericTrader> trader_ptr,
+    const std::string& algo_id, const std::string& logfile_url
 )
 {
-    auto timer = ba::steady_timer{io_context_, std::chrono::seconds(time_s)};
+    auto timer = ba::steady_timer{io_context_, std::chrono::seconds(time_ms)};
 
-    timer.async_wait([trader_ptr](const boost::system::error_code& err_code) {
+    timer.async_wait([trader_ptr, logfile_url,
+                      algo_id](const boost::system::error_code& err_code) {
         auto trader = trader_ptr.lock();
         if (trader == nullptr) {
-            log_i(
-                sandbox_server, "Trader already removed: {}", trader->get_display_name()
-            );
+            log_i(sandbox_server, "Trader already removed: {}", algo_id);
             return;
         }
         if (err_code) {
@@ -134,6 +148,19 @@ CrowServer::start_remove_timer_(
             log_i(sandbox_server, "Removing trader {}", trader->get_display_name());
             trader->disable();
         }
+
+        std::ifstream log_file(fmt::format("{}/{}.log", LOG_DIR, algo_id));
+        if (!log_file.is_open()) {
+            log_e(
+                sandbox_server, "Unable to open log file for trader {}",
+                trader->get_display_name()
+            );
+            return;
+        }
+        std::stringstream log_ss;
+        log_ss << log_file.rdbuf();
+
+        client::put_request(logfile_url, log_ss.str());
     });
     timers_.push_back(std::move(timer));
 }
--- web/app/dash/submissions/[id]/graphs.tsx
+++ web/app/dash/submissions/[id]/graphs.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { ArrowDownTrayIcon } from "@heroicons/react/16/solid";
 import { Algo, AlgoFile } from "@prisma/client";
 import React, { useEffect, useState } from "react";
 
 export function AlgoGraphs({
   algo,
   userId,
+  s3Endpoint,
 }: {
   algo: Algo & { algoFile: AlgoFile };
   userId: string;
+  s3Endpoint: string;
 }) {
+  const [showLogs, setShowLogs] = useState(false);
   const upTime = new Date(algo?.algoFile?.createdAt).getTime() + 1000;
   const sandboxTimeMs = 300000;
   const baseEndpoint = `${process.env.NEXT_PUBLIC_NGINX_ENDPOINT}/d-solo/cdk4teh4zl534a/nutc-dev?orgId=1&var-traderid=${algo.algoFileS3Key}&from=${upTime}&theme=dark`;
@@ -20,14 +24,27 @@ export function AlgoGraphs({
       setUrl(baseEndpoint + "&refresh=5s");
       setTimeout(() => {
         setUrl(baseEndpoint + `&to=${upTime + sandboxTimeMs}`);
+        setShowLogs(true);
       }, upTime + sandboxTimeMs - Date.now());
     } else {
       setUrl(baseEndpoint + `&to=${upTime + sandboxTimeMs}`);
+      setShowLogs(true);
     }
-  }, [baseEndpoint, upTime]);
+  }, [baseEndpoint, setShowLogs, upTime]);
 
   return (
     <>
+      {showLogs ? (
+        <a
+          type="button"
+          target="_blank"
+          href={`${s3Endpoint}/nutc/${algo.logFileS3Key}`}
+          className="mt-2 inline-flex items-center gap-x-1.5 rounded-md bg-indigo-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700">
+          Download Logs
+          <ArrowDownTrayIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </a>
+      ) : null}
+
       <div className="my-8">
         <h2 className="text-xl font-semibold mb-2">Profit and Loss</h2>
         <iframe
