61d1769b0fe799fc8606efdf9a0d6b8d2afa03a1
Steven Ewald <stevenewald6@gmail.com>
2025-01-09 10:47:51 -0600

    Add exchange_state

--- exchange/src/exchange/matching_cycle/dev/dev_cycle.hpp
+++ exchange/src/exchange/matching_cycle/dev/dev_cycle.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "exchange/exchange_state.hpp"
 #include "exchange/matching_cycle/base/base_cycle.hpp"
 #include "exchange/metrics/on_tick_metrics.hpp"
 #include "exchange/traders/trader_container.hpp"
@@ -14,11 +15,9 @@ class DevMatchingCycle : public BaseMatchingCycle {
 
 public:
     DevMatchingCycle(
-        TickerContainer tickers, TraderContainer& traders,
-        common::decimal_price order_fee, common::decimal_quantity max_order_volume
-    ) :
-        BaseMatchingCycle(std::move(tickers), traders, order_fee, max_order_volume),
-        pusher(traders)
+        exchange_state& state, common::decimal_price order_fee,
+        common::decimal_quantity max_order_volume
+    ) : BaseMatchingCycle(state, order_fee, max_order_volume), pusher(state.traders)
     {}
 
 protected:
